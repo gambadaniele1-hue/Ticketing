a915f9cba51f8e5d7bfbbfd71585c9013fd6106f
Daniele Gamba <gamba.daniele.studente@itispaleocapa.it>
2026-05-11 18:37:11 +0200

    fix: cors.php

--- config/cors.php
+++ config/cors.php
@@ -10,10 +10,10 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
     ],
 
-    'allowed_origins_patterns' => [],
+
+    'allowed_origins_patterns' => ['#^http://.*\.localhost:5173$#'],
 
     'allowed_headers' => ['*'],
 
